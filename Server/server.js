f907024da7aef1e6a5291fbd6c0b39d5bdbc9378
aeliskothiya <22bmiit009@gmail.com>
2025-11-08 12:09:00 +0530

    cloud

--- Server/server.js
+++ Server/server.js
@@ -29,7 +29,11 @@ const __dirname = path.dirname(__filename);
 
 connectdb();
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5175',process.env.FRONTEND_URL ];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://technosys.vercel.app"
+];
+
 
 // Sanitize data for mongoose injection
 // app.use(mongoSanitize());
